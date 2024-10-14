db35a271fb43076431ca01e99d23dbd31b818ab5
KricsDavid <david@kricsfalusi.hu>
2024-10-14 08:34:57 +0200

    list

---
+++ ViccAdatbazis/Controllers/ViccController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViccAdatbazis.Data;
+using ViccAdatbazis.Models;
+
+namespace ViccAdatbazis.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViccController : Controller
+    {
+
+        //vicc listazas
+        [HttpGet]
+        public async Task<ActionResult<List<Vicc>>> GetViccek() { 
+            return await _context.Viccek.Where(x => x.Aktiv).ToListAsync();
+        }
+        public ViccController(ViccDbContext context) 
+        { 
+            _context = context;
+        }
+
+        //list
+
+        [HttpGet]
+
+        
+
+
+        //lekeres
+        [HttpGet("{id}")]
+
+    }   
+}
