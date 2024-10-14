41e9f03c21efa886359bd358fdfe1c2e23951e73
KricsDavid <david@kricsfalusi.hu>
2024-10-14 08:51:59 +0200

    modsitas feltoltes

--- ViccAdatbazis/Controllers/ViccController.cs
+++ ViccAdatbazis/Controllers/ViccController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ViccAdatbazis.Data;
 using ViccAdatbazis.Models;
 
@@ -9,6 +10,7 @@ namespace ViccAdatbazis.Controllers
     [ApiController]
     public class ViccController : Controller
     {
+        private readonly ViccDbContext _context;
 
         //vicc listazas
         [HttpGet]
@@ -29,6 +31,39 @@ namespace ViccAdatbazis.Controllers
 
         //lekeres
         [HttpGet("{id}")]
+
+        public async Task<ActionResult<Vicc>> GetVicc(int id) 
+        {
+            var vicc = _context.Viccek.Find(id);
+            if (vicc == null) {return NotFound();}
+            return vicc; 
+        }
+
+        //feltoltes
+
+        [HttpPost]
+        public async Task<ActionResult> PostVicc(Vicc feltoltottVicc) 
+        {
+            _context.Viccek.Add(feltoltottVicc);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        //modositas
+
+        [HttpPut("{id}")]
+
+
+        public async Task<ActionResult> PutVicc(int id, Vicc modositottVicc)
+        {
+            //var vicc = _context.Viccek.Find(id);
+            if (id != modositottVicc.Id) { return BadRequest(); }
+            _context.Entry(modositottVicc).State = EntityState.Modified;
+             await _context.SaveChangesAsync();
+
+            return NoContent(); 
+        }
+
 
     }   
 }
